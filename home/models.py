b739e2e9424f97fc5296a515f0c70378849bd789
clever-king-402 <122508802+clever-king-402@users.noreply.github.com>
2025-04-25 15:51:42 +0545

    data fetched into database

--- home/models.py
+++ home/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+# id
+# city
+# country
+# latitude
+# logintude
+# temperature
+# weather_desciption
+# humidity
+# wind_speed
 # Create your models here.
+class Temperature(models.Model):
+    city = models.CharField(max_length=100,)
+    country = models.CharField(max_length=100)
+    latitude = models.CharField(max_length=500)
+    logintude = models.CharField(max_length=100)
+    weather_description = models.CharField(max_length=100)
+    humitidy = models.CharField(max_length=100)
+    temperature =models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.city;
--- home/views.py
+++ home/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+import requests
+from .models import *
 
 # Create your views here.
 def home(request):
-    return render(request, "index.html")
+    # this is for initial fetching
+    # url = "https://freetestapi.com/api/v1/weathers"
+    # response = {}
+    # response["response"] = requests.get(url).json()
+    response = {}
+    response["response"] = Temperature.objects.all()[:10]
+    return render(request, "index.html",response)
+
+# these are keys
+# id
+# city
+# country
+# latitude
+# logintude
+# temperature
+# weather_desciption
+# humidity
+# wind_speed
+# forecast
